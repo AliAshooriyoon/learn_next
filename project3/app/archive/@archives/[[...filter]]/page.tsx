e38aa3973cc8d36f27fb72ee12ced16de854fb77
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-06-09 22:17:05 +0330

    edit header

--- project3/app/archive/@archives/[[...filter]]/page.tsx
+++ project3/app/archive/@archives/[[...filter]]/page.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { newsData } from "@/app/data";
-import Image from "next/image";
 const YearArchives = ({ params }) => {
 
   const news = newsData;
@@ -15,21 +14,21 @@ const YearArchives = ({ params }) => {
       <div className="flex items-center justify-center gap-16 text-center mx-auto">
         This Year :
         <div className="flex items-center justify-center gap-16">
-          all Archives
           <Link href={'/'} >Haus</Link>
+          <Link href={'/archive'} >Archives</Link>
           {news.map(i => <div className="flex" key={i.newID}>
             <Link className="cursor-pointer" href={`/archive/${i.eventDate[2]}`}>Year {i.eventDate[2]}</Link>
           </div>)}
         </div>
       </div>
       {findedData && <div className="">
-        {findedData.map(elm => <div key={elm.newID} className="w-[90%]  bg-gray-800 mx-auto mt-12 rounded-2xl p-4">
+        {findedData.map(elm => <div key={elm.newID} className="w-[90%]  bg-gray-800 mx-auto mt-12 rounded-2xl p-12">
           <img className="rounded-2xl w-full h-96" src={elm.newPic} alt='pic' />
           <p className="">Date : {elm.eventDate[0]}.  {elm.eventDate[1]}.  {elm.eventDate[2]}</p>
-          <div className="main flex flex-col gap-12 pt-12 text-left indent-3">
+          <div className="main flex flex-col gap-12 pt-12 text-left indent-3 max-w-[95%]  mx-auto">
             <p className='title text-2xl text-center'> {elm.newTitle} </p>
             <p className='short_details text-base text-green-200'> {elm.newShortDetails} </p>
-            <p className="more_details text-lg"> {elm.newFullDetails} </p>
+            <p className="more_details text-lg "> {elm.newFullDetails} </p>
           </div>
         </div>)}
       </div>}
--- project3/app/archive/@latest/default.tsx
+++ project3/app/archive/@latest/default.tsx
@@ -1,6 +1,6 @@
 const DefaultLatest = () => {
   return (<>
-    <div className="">Latest News</div>
+    <div className="w-32 h-32 bg-cyan-800 mx-auto my-12 text-center">Latest News</div>
   </>)
 }
 export default DefaultLatest;
