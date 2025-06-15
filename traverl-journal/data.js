0c3759d1b03a303cc4c50e3b49c0f5f97c66ce8a
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-06-15 16:40:40 +0330

    write travel data

---
+++ traverl-journal/data.js
@@ -0,0 +1,11 @@
+export const traverData = [
+  {
+    id:1,
+    traverlName:"Reise nach Österreich",
+    travelShortDetails:
+    "Diese ist eine Reise nach Österreich, wo ich dauerhaft fahren mochte",
+    travelDetails:"Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses, wobei die ersten Menschen heute nicht sind. Zudem sind Beispiele jetzt noch frei, denn man sollte bedenken, der Text macht wenig Sinn. Mit brausender Geschwindigkeit war der Titel des neuen Boots in naher Ferne, weshalb sich der Briefträger einen roten Mantel zur Reinigung brachte. Ein weiterer Punkt wäre es, die verständliche Blindtexte heute noch anzurufen denn der Tisch steht im Gang. Zur damaligen Zeit war es eher üblich, die gebrauchten Handtücher mittels Fensterreiniger auszulüften, um auch die Wanduhr als Beispiel",
+    slug:"travel-austria-2022",
+    date:[12, 4, 2024]
+  },
+]
