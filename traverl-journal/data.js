106e4866cb115cca057e9d9a76ce2772f9769944
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-06-15 17:02:59 +0330

    update travel data

--- traverl-journal/data.js
+++ traverl-journal/data.js
@@ -1,11 +1,100 @@
 export const traverData = [
   {
     id:1,
-    traverlName:"Reise nach Österreich",
+    travelName:"Reise nach Österreich",
     travelShortDetails:
     "Diese ist eine Reise nach Österreich, wo ich dauerhaft fahren mochte",
     travelDetails:"Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses, wobei die ersten Menschen heute nicht sind. Zudem sind Beispiele jetzt noch frei, denn man sollte bedenken, der Text macht wenig Sinn. Mit brausender Geschwindigkeit war der Titel des neuen Boots in naher Ferne, weshalb sich der Briefträger einen roten Mantel zur Reinigung brachte. Ein weiterer Punkt wäre es, die verständliche Blindtexte heute noch anzurufen denn der Tisch steht im Gang. Zur damaligen Zeit war es eher üblich, die gebrauchten Handtücher mittels Fensterreiniger auszulüften, um auch die Wanduhr als Beispiel",
     slug:"travel-austria-2022",
     date:[12, 4, 2024]
+  },
+  {
+    id:2,
+    travelName:"Reise nach Deutschland",
+    travelShortDetails:
+    "Diese ist eine Reise nach Deutschland, wo ich dauerhaft fahren mochte",
+    travelDetails:"Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses, wobei die ersten Menschen heute nicht sind. Zudem sind Beispiele jetzt noch frei, denn man sollte bedenken, der Text macht wenig Sinn. Mit brausender Geschwindigkeit war der Titel des neuen Boots in naher Ferne, weshalb sich der Briefträger einen roten Mantel zur Reinigung brachte. Ein weiterer Punkt wäre es, die verständliche Blindtexte heute noch anzurufen denn der Tisch steht im Gang. Zur damaligen Zeit war es eher üblich, die gebrauchten Handtücher mittels Fensterreiniger auszulüften, um auch die Wanduhr als Beispiel",
+    slug:"travel-germany-2022",
+    date:[12, 5, 2024]
+  },{
+    id:3,
+    travelName:"Reise nach Danmerk",
+    travelShortDetails:
+    "Diese ist eine Reise nach Danmerk, wo ich dauerhaft fahren mochte",
+    travelDetails:"Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses, wobei die ersten Menschen heute nicht sind. Zudem sind Beispiele jetzt noch frei, denn man sollte bedenken, der Text macht wenig Sinn. Mit brausender Geschwindigkeit war der Titel des neuen Boots in naher Ferne, weshalb sich der Briefträger einen roten Mantel zur Reinigung brachte. Ein weiterer Punkt wäre es, die verständliche Blindtexte heute noch anzurufen denn der Tisch steht im Gang. Zur damaligen Zeit war es eher üblich, die gebrauchten Handtücher mittels Fensterreiniger auszulüften, um auch die Wanduhr als Beispiel",
+    slug:"travel-danmerk-2023",
+    date:[2, 4, 2023]
+  },{
+    id:4,
+    travelName:"Reise in die Schweiz",
+    travelShortDetails:
+    "Diese ist eine Reise in die Schweiz, wo ich dauerhaft fahren mochte",
+    travelDetails:"Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses, wobei die ersten Menschen heute nicht sind. Zudem sind Beispiele jetzt noch frei, denn man sollte bedenken, der Text macht wenig Sinn. Mit brausender Geschwindigkeit war der Titel des neuen Boots in naher Ferne, weshalb sich der Briefträger einen roten Mantel zur Reinigung brachte. Ein weiterer Punkt wäre es, die verständliche Blindtexte heute noch anzurufen denn der Tisch steht im Gang. Zur damaligen Zeit war es eher üblich, die gebrauchten Handtücher mittels Fensterreiniger auszulüften, um auch die Wanduhr als Beispiel",
+    slug:"travel-schweiz-2025",
+    date:[23, 7, 2025]
+  },{
+    id:5,
+    travelName:"Reise nach China",
+    travelShortDetails:
+    "Diese ist eine Reise nach China, wo ich dauerhaft fahren mochte",
+    travelDetails:"Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses, wobei die ersten Menschen heute nicht sind. Zudem sind Beispiele jetzt noch frei, denn man sollte bedenken, der Text macht wenig Sinn. Mit brausender Geschwindigkeit war der Titel des neuen Boots in naher Ferne, weshalb sich der Briefträger einen roten Mantel zur Reinigung brachte. Ein weiterer Punkt wäre es, die verständliche Blindtexte heute noch anzurufen denn der Tisch steht im Gang. Zur damaligen Zeit war es eher üblich, die gebrauchten Handtücher mittels Fensterreiniger auszulüften, um auch die Wanduhr als Beispiel",
+    slug:"travel-china-2019",
+    date:[29, 11, 2019]
+  }, {
+    id:6,
+    travelName:"Reise nach Japan",
+    travelShortDetails:
+    "Diese ist eine Reise nach Japan, wo ich dauerhaft fahren mochte",
+    travelDetails:"Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses, wobei die ersten Menschen heute nicht sind. Zudem sind Beispiele jetzt noch frei, denn man sollte bedenken, der Text macht wenig Sinn. Mit brausender Geschwindigkeit war der Titel des neuen Boots in naher Ferne, weshalb sich der Briefträger einen roten Mantel zur Reinigung brachte. Ein weiterer Punkt wäre es, die verständliche Blindtexte heute noch anzurufen denn der Tisch steht im Gang. Zur damaligen Zeit war es eher üblich, die gebrauchten Handtücher mittels Fensterreiniger auszulüften, um auch die Wanduhr als Beispiel",
+    slug:"travel-japan-2024",
+    date:[12, 4, 2024]
+  },{
+    id:7,
+    travelName:"Reise nach India",
+    travelShortDetails:
+    "Diese ist eine Reise nach India, wo ich dauerhaft fahren mochte",
+    travelDetails:"Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses, wobei die ersten Menschen heute nicht sind. Zudem sind Beispiele jetzt noch frei, denn man sollte bedenken, der Text macht wenig Sinn. Mit brausender Geschwindigkeit war der Titel des neuen Boots in naher Ferne, weshalb sich der Briefträger einen roten Mantel zur Reinigung brachte. Ein weiterer Punkt wäre es, die verständliche Blindtexte heute noch anzurufen denn der Tisch steht im Gang. Zur damaligen Zeit war es eher üblich, die gebrauchten Handtücher mittels Fensterreiniger auszulüften, um auch die Wanduhr als Beispiel",
+    slug:"travel-india-2020",
+    date:[3, 7, 2020]
+  },{
+    id:8,
+    travelName:"Reise nach Kanada",
+    travelShortDetails:
+    "Diese ist eine Reise nach Kanada, wo ich dauerhaft fahren mochte",
+    travelDetails:"Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses, wobei die ersten Menschen heute nicht sind. Zudem sind Beispiele jetzt noch frei, denn man sollte bedenken, der Text macht wenig Sinn. Mit brausender Geschwindigkeit war der Titel des neuen Boots in naher Ferne, weshalb sich der Briefträger einen roten Mantel zur Reinigung brachte. Ein weiterer Punkt wäre es, die verständliche Blindtexte heute noch anzurufen denn der Tisch steht im Gang. Zur damaligen Zeit war es eher üblich, die gebrauchten Handtücher mittels Fensterreiniger auszulüften, um auch die Wanduhr als Beispiel",
+    slug:"travel-kanada-2020",
+    date:[1, 9, 2020]
+  }, {
+    id:9,
+    travelName:"Reise in den Irak",
+    travelShortDetails:
+    "Diese ist eine Reise in den Irak, wo ich dauerhaft fahren mochte",
+    travelDetails:"Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses, wobei die ersten Menschen heute nicht sind. Zudem sind Beispiele jetzt noch frei, denn man sollte bedenken, der Text macht wenig Sinn. Mit brausender Geschwindigkeit war der Titel des neuen Boots in naher Ferne, weshalb sich der Briefträger einen roten Mantel zur Reinigung brachte. Ein weiterer Punkt wäre es, die verständliche Blindtexte heute noch anzurufen denn der Tisch steht im Gang. Zur damaligen Zeit war es eher üblich, die gebrauchten Handtücher mittels Fensterreiniger auszulüften, um auch die Wanduhr als Beispiel",
+    slug:"travel-irak-2017",
+    date:[6, 12, 2017]
+  },{
+    id:10,
+    travelName:"Reise nach Gerichland",
+    travelShortDetails:
+    "Diese ist eine Reise nach Gerichland, wo ich dauerhaft fahren mochte",
+    travelDetails:"Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses, wobei die ersten Menschen heute nicht sind. Zudem sind Beispiele jetzt noch frei, denn man sollte bedenken, der Text macht wenig Sinn. Mit brausender Geschwindigkeit war der Titel des neuen Boots in naher Ferne, weshalb sich der Briefträger einen roten Mantel zur Reinigung brachte. Ein weiterer Punkt wäre es, die verständliche Blindtexte heute noch anzurufen denn der Tisch steht im Gang. Zur damaligen Zeit war es eher üblich, die gebrauchten Handtücher mittels Fensterreiniger auszulüften, um auch die Wanduhr als Beispiel",
+    slug:"travel-gerichland-2023",
+    date:[27, 11, 2023]
+  },{
+    id:11,
+    travelName:"Reise in die Türkei",
+    travelShortDetails:
+    "Diese ist eine Reise in die Türkei, wo ich dauerhaft fahren mochte",
+    travelDetails:"Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses, wobei die ersten Menschen heute nicht sind. Zudem sind Beispiele jetzt noch frei, denn man sollte bedenken, der Text macht wenig Sinn. Mit brausender Geschwindigkeit war der Titel des neuen Boots in naher Ferne, weshalb sich der Briefträger einen roten Mantel zur Reinigung brachte. Ein weiterer Punkt wäre es, die verständliche Blindtexte heute noch anzurufen denn der Tisch steht im Gang. Zur damaligen Zeit war es eher üblich, die gebrauchten Handtücher mittels Fensterreiniger auszulüften, um auch die Wanduhr als Beispiel",
+    slug:"travel-turkei-2022",
+    date:[12, 8, 2022]
+  },{
+    id:12,
+    travelName:"Reise nach Oman",
+    travelShortDetails:
+    "Diese ist eine Reise nach Oman, wo ich dauerhaft fahren mochte",
+    travelDetails:"Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses, wobei die ersten Menschen heute nicht sind. Zudem sind Beispiele jetzt noch frei, denn man sollte bedenken, der Text macht wenig Sinn. Mit brausender Geschwindigkeit war der Titel des neuen Boots in naher Ferne, weshalb sich der Briefträger einen roten Mantel zur Reinigung brachte. Ein weiterer Punkt wäre es, die verständliche Blindtexte heute noch anzurufen denn der Tisch steht im Gang. Zur damaligen Zeit war es eher üblich, die gebrauchten Handtücher mittels Fensterreiniger auszulüften, um auch die Wanduhr als Beispiel",
+    slug:"travel-austria-2024",
+    date:[3, 2, 2024]
   },
 ]
