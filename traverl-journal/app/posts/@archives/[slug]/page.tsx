1b97f8952c1f98fe34f3946e600ad3a367704d07
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-06-17 10:10:30 +0330

    develop slug part

--- traverl-journal/app/posts/@archives/[slug]/page.tsx
+++ traverl-journal/app/posts/@archives/[slug]/page.tsx
@@ -1,8 +1,28 @@
+"use client"
+import { travelData } from "@/data";
+import { useState, useEffect } from "react"
 const ArchivePost = ({ params }) => {
+  const [currentPost, setCurrentpost] = useState(null)
+  useEffect(() => {
+    setCurrentpost(travelData.find((i) => i.slug == params.slug))
+  }, [])
+
+  useEffect(
+    () => {
+      console.log(currentPost)
+    }, [currentPost]
+
+  )
   return (
     <>
-      <div className="">Archives</div>
-      <p className=''>{params.slug}</p>
+      <div className="bg-amber-700 min-w-64 min-h-32 mx-auto flex flex-col gap-6
+        items-center justify-center text-2xl rounded-2xl p-4">
+        <p className=''>{params.slug}</p>
+        {currentPost && <div className="flex flex-col items-center justify-center gap-6 text-lg text-center">
+          <p className="">{currentPost.travelShortDetails}</p>
+          <p className="">{currentPost.travelDetails}</p>
+        </div>}
+      </div>
     </>
   )
 }
--- traverl-journal/app/posts/@archives/page.tsx
+++ traverl-journal/app/posts/@archives/page.tsx
@@ -1,9 +1,21 @@
+"use client"
+import Link from "next/link";
+import { useEffect, useState } from "react"
+import { travelData } from "@/data"
 const Archive = () => {
+  console.log(travelData)
+  const [data, setData] = useState([])
+  useEffect(() => {
+    setData(travelData);
+
+  }, [])
   return (
     <>
-      <div className="">
-        Archive
-
+      <div className="mx-auto">
+        Archives:
+        <div className="flex flex-col gap-4 mx-auto items-center justify-center">
+          {data?.map((i) => <div className=""> <Link href={`/posts/${i.slug}`}> {i.travelName} </Link> </div>)}
+        </div>
       </div>
     </>
   )
