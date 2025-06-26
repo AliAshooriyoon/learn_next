45c2720f76edd82e3a669a7e3eecb319048f6158
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-06-26 18:43:01 +0330

    some changes

--- traverl-journal/app/component/Header.tsx
+++ traverl-journal/app/component/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Header = () => {
   return <>
-    <div className="w-full h-16 bg-cyan-600 flex items-center ">
+    <div className="w-full h-16 bg-cyan-600 flex items-center">
       <div className="flex mx-auto gap-24 text-2xl">
         <span className="cursor-pointer">  <Link href={'/'}> Haus </Link> </span>
         <span className="cursor-pointer"><Link href={'/posts'}> Posts </Link></span>
--- traverl-journal/app/posts/@archives/[[...slug]]/page.tsx
+++ traverl-journal/app/posts/@archives/[[...slug]]/page.tsx
@@ -2,12 +2,14 @@
 import { travelData } from "@/data";
 import { useState, useEffect } from "react"
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { notFound, usePathname } from "next/navigation";
 const ArchivePost = ({ params }) => {
   const [currentPost, setCurrentpost] = useState(null)
   const path = usePathname()
   const data = travelData;
   const isHereJustPosts = path == '/posts';
+  console.log(!isHereJustPosts)
+  console.log(currentPost)
   useEffect(() => {
     // travelData.find((i) => i.slug == params.slug[0])
     setCurrentpost(!isHereJustPosts && travelData.find((i) => i.slug == params.slug[0])
@@ -16,6 +18,7 @@ const ArchivePost = ({ params }) => {
 
   useEffect(
     () => {
+
     }, [currentPost]
 
   )
@@ -24,14 +27,15 @@ const ArchivePost = ({ params }) => {
       <div className="bg-cyan-900 max-w-[70%] min-w-64 min-h-32 mx-auto flex flex-col gap-6
         items-center justify-center text-3xl rounded-2xl p-8">
 
-        {isHereJustPosts && data?.map((i) => <div className="">
-          <Link href={`/posts/${i.slug}`}> {i.travelName} </Link> </div>)}
-        <p className=''>{currentPost?.travelName}</p>
-        {currentPost && <div className="flex flex-col  gap-12 text-lg text-left">
-          <p className="text-2xl">{currentPost.travelShortDetails}</p>
-          <p className="text-lg">{currentPost.travelDetails}</p>
-          <p className="text-left text-xl"> Date: {currentPost.date[0]}.{currentPost.date[1]}.{currentPost.date[2]} </p>
-        </div>}
+        {isHereJustPosts ? data?.map((i) => <div className="">
+          <Link href={`/posts/${i.slug}`}> {i.travelName} </Link> </div>) : currentPost ? <div>
+            <p className=''>{currentPost?.travelName}</p>
+            <div className="flex flex-col  gap-12 text-lg text-left">
+              <p className="text-2xl">{currentPost.travelShortDetails}</p>
+              <p className="text-lg">{currentPost.travelDetails}</p>
+              <p className="text-left text-xl"> Date: {currentPost.date[0]}.{currentPost.date[1]}.{currentPost.date[2]} </p>
+            </div>
+          </div> : <p>Error</p>}
       </div>
     </>
   )
